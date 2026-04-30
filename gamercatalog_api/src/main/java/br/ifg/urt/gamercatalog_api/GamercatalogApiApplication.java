d5c21b9c25dc977c9c67005325938ba41181c5db
João Pedro <joaopedromartins087@gmail.com>
2026-04-29 22:56:09 -0300

    inicio

---
+++ gamercatalog_api/src/main/java/br/ifg/urt/gamercatalog_api/GamercatalogApiApplication.java
@@ -0,0 +1,13 @@
+package br.ifg.urt.gamercatalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamercatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamercatalogApiApplication.class, args);
+	}
+
+}
---
+++ gamercatalog_api/src/test/java/br/ifg/urt/gamercatalog_api/GamercatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifg.urt.gamercatalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamercatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
