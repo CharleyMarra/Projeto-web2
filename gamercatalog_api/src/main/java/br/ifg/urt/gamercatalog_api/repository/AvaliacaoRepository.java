1c1be83ff1c939974fd743f5a2337596a558eaa1
João Pedro <joaopedromartins087@gmail.com>
2026-05-28 23:14:57 -0300

    AvaliacaoRepository

--- gamercatalog_api/src/main/java/br/ifg/urt/gamercatalog_api/repository/AvaliacaoRepository.java
+++ gamercatalog_api/src/main/java/br/ifg/urt/gamercatalog_api/repository/AvaliacaoRepository.java
@@ -1,7 +1,7 @@
 package br.ifg.urt.gamercatalog_api.repository;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.ifg.urt.gamercatalog_api.model.Avaliacao;
